cda5e1f0ca46cae50df1c21946a02e662651e885
Danial Shakeri <dasha@Danials-MacBook-Pro.local>
2023-04-17 16:55:35 -0700

    DB/Table will now be automatically created, so only Sql needs to be installed and started

--- Java-Demo-Project/src/main/java/com/CCIjavaDemo/TextDAO.java
+++ Java-Demo-Project/src/main/java/com/CCIjavaDemo/TextDAO.java
@@ -9,17 +9,60 @@ import java.sql.Statement;
 
 public class TextDAO {
 
-	private String jdbcURL = "jdbc:mysql://localhost:3306/demo";
+	private String jdbcURLNoDB = "jdbc:mysql://localhost/";
+	private String dbName = "DemoDb";
+	private String tableName = "DemoTable";
+	private String jdbcURL = "jdbc:mysql://localhost:3306/" + dbName;
 	private String jdbcUsername = "root";
 	private String jdbcPassword = "";
 	private Connection jdbcConnection;
 	
+	 public TextDAO() {
+		 try {
+			createDBAndTableIfDoesNotExist();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	 }
+	 
+	private void createDBAndTableIfDoesNotExist() throws ClassNotFoundException {
+		// Create DB if it doesn't exist
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(jdbcURLNoDB, jdbcUsername, jdbcPassword);
+		    Statement stmt = conn.createStatement();
+		    stmt.executeUpdate("CREATE DATABASE IF NOT EXISTS " + dbName);
+		    stmt.close();
+		    conn.close();
+		} catch (SQLException e) {
+		    e.printStackTrace();
+		} 
+		
+		// Create table if it doesn't exist
+		createTable();
+	}
+	
+	private void createTable() {
+		connect(); 
+		String createTableQuery = String.format("CREATE TABLE IF NOT EXISTS %s(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(500) NOT NULL)", tableName);
+		String addTableEntry = String.format("INSERT INTO %s(text) SELECT (\"Trial Text\") WHERE NOT EXISTS (SELECT * FROM %s)", tableName, tableName);
+		try {
+			PreparedStatement statement = jdbcConnection.prepareStatement(createTableQuery);
+			statement.executeUpdate(); 
+			statement = jdbcConnection.prepareStatement(addTableEntry);
+			statement.executeUpdate();
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		disconnect();
+	}
+	
 	public void connect() {
 		try {
 			if (jdbcConnection == null || jdbcConnection.isClosed()) {
-				Class.forName("com.mysql.jdbc.Driver");
+				Class.forName("com.mysql.cj.jdbc.Driver");
 				jdbcConnection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
-				System.out.println("MySQL Connection Established");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -41,7 +84,7 @@ public class TextDAO {
 		String text = null;
 		try {
 			Statement statement = jdbcConnection.createStatement();
-			ResultSet result = statement.executeQuery("Select * FROM text");
+			ResultSet result = statement.executeQuery("Select * FROM " + tableName);
 			while (result.next()) {
 				text = result.getString("text");
 			}
@@ -56,11 +99,10 @@ public class TextDAO {
 	
 	public boolean updateText(String newText) {
 		connect(); 
-		String sql = "UPDATE text SET text = ?";
+		String sql = String.format("UPDATE %s SET text = \'%s\'", tableName, newText);
 		boolean rowUpdated = false;
 		try {
 			PreparedStatement statement = jdbcConnection.prepareStatement(sql);
-			statement.setString(1, newText);
 			rowUpdated = statement.executeUpdate() > 0;
 			statement.close();
 		} catch (SQLException e) {
